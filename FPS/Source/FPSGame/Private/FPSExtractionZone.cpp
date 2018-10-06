64ed68981c5e183f16fae6f8cb50a97bad6267cf
NeoHSK <mistgrin@gmail.com>
2018-10-06 19:35:23 +0900

    Add Extracation Feedback

--- FPS/Source/FPSGame/Private/FPSExtractionZone.cpp
+++ FPS/Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -1,7 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "FPSExtractionZone.h"
+#include "FPSCharacter.h"
+#include "FPSGameMode.h"
 #include "Components/BoxComponent.h" 
+#include "Components/DecalComponent.h"
+#include "Kismet//GameplayStatics.h"
 
 // Sets default values
 AFPSExtractionZone::AFPSExtractionZone()
@@ -16,8 +20,13 @@ AFPSExtractionZone::AFPSExtractionZone()
 	OverlapComp->SetBoxExtent(FVector(200.0f));
 	RootComponent = OverlapComp;
 
+	OverlapComp->SetHiddenInGame(false);
+
 	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
 
+	DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("DecolComp"));
+	DecalComp->DecalSize = FVector(200.0f);
+	DecalComp->SetupAttachment(OverlapComp);
 }
 
 // Called when the game starts or when spawned
@@ -29,6 +38,26 @@ void AFPSExtractionZone::BeginPlay()
 
 void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
+	AFPSCharacter* MyPawn = Cast<AFPSCharacter>(OtherActor);
+
+	if(MyPawn == nullptr)
+	{
+		return;
+	}
+
+	if (MyPawn->bIsCarryingObjective)
+	{
+		AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
+		if (GM)
+		{
+			GM->CompleteMission(MyPawn);
+		}
+	}
+	else
+	{
+		UGameplayStatics::PlaySound2D(this, ObjectiveMissiingSound);
+	}
+
 	UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone!"));
 }
 
--- FPS/Source/FPSGame/Public/FPSExtractionZone.h
+++ FPS/Source/FPSGame/Public/FPSExtractionZone.h
@@ -7,6 +7,7 @@
 #include "FPSExtractionZone.generated.h"
 
 class UBoxComponent;
+//class UDecol
 
 UCLASS()
 class FPSGAME_API AFPSExtractionZone : public AActor
@@ -23,6 +24,12 @@ protected:
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UBoxComponent* OverlapComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UDecalComponent* DecalComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Sounds")
+	USoundBase* ObjectiveMissiingSound;
 
 	UFUNCTION()
 	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
